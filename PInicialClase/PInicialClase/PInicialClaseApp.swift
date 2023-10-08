bb4be03376a3ad63aa854955bc99fcb4c9d81f14
Jose David Ramirez Hernandez <davidramirez0498@gmail.com>
2023-10-08 09:16:43 -0700

    comit inicial

---
+++ PInicialClase/PInicialClase/PInicialClaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PInicialClaseApp.swift
+//  PInicialClase
+//
+//  Created by JOSE DAVID RAMIREZHERNANDEZ on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PInicialClaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
