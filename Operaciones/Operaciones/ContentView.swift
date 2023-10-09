f382835402ff4b67d20d6401a926847dbb2f888a
Jose David Ramirez Hernandez <davidramirez0498@gmail.com>
2023-10-08 18:21:23 -0700

    comit

--- Operaciones/Operaciones/ContentView.swift
+++ Operaciones/Operaciones/ContentView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var Texto="Saludos"
-    @State private var Nombre="William ernesto munoz Raymundo"
+    @State private var Texto="Saluditos"
+    @State private var Nombre="Jose David Ramirez Hernandez"
     @State private var myInteger = 1
     @State private var variableA = 2
     @State private var variableB = 3
@@ -18,11 +18,16 @@ struct ContentView: View {
         VStack {
             Text("\(Texto), \(Nombre)")
                 .padding()
-            
             //Text("\(myInteger)")
            // OutSourceButtonView(myInteger: $myInteger)
             Text("la suma de las variables \(variableA) + \(variableB) es: \(variableA+variableB)")
         }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
@@ -42,8 +47,3 @@ struct ContentView: View {
     <#fields#>
 }*/
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
